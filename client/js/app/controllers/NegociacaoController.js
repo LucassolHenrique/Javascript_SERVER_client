04c168bc561f1157334c69f16ace7247e579d050
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2024-06-13 08:34:26 -0300

    nova pagina criada
    a tabela se limpa sozinha depois de criar uma tabela e tambem ja tenha o mouse selecionado no lugar correto para escrever

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,28 +9,39 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
         // esta sendo usado o constructor para deixar mais rapido o site
         // na hora de pesquisar o codigo
     }
 
     adiciona(event) {
         
-        event.preventDefault()
+        event.preventDefault();
 
-        let negociacao = new Negociacao(
+        //console.log(negociacao);
+        //console.log(DateHelper.dataParaTexto(negociacao.data));
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+
+
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value 
-        );
-        
+            this._inputValor.value); 
 
-        console.log(negociacao);
-        
-        console.log(DateHelper.dataParaTexto(negociacao.data));
-        
-        
+    } 
+    
+    _limpaFormulario() {
 
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
 
+        this._inputData.focus();
 
     }
 
