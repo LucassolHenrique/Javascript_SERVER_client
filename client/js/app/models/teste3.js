70d89764e4ca698d3aed1b436237e310f55e6362
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2024-06-18 13:41:34 -0300

    codigos teste expressao
    testando uma maneira de fazer uma expressão /^\D{3}-\D{2}-\d{2}$/
    \D é qualquer coisa não dígito
    \D{3} é qualquer coisa não dígito que forme um grupo de 3 caracteres
    // \d é qualquer dígito.
    tem que ser o numero exato de caracteres $^

--- client/js/app/models/teste3.js
+++ client/js/app/models/teste3.js
@@ -1,29 +1,51 @@
 
 
-validaCodigo('GWw-Jj-12'); // válido
+class negociacao {
+
+    constructor() {
+        this._negociacoes = [];
+    }
+
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+
+    get negociacoes() {
+        return this._negociacoes;
+    }
+}
+
+
+
+
+
+
+
+
+//validaCodigo('GWZ-JJ-12'); // válido
 //validaCodigo('1X1-JJ-12'); // inválido
 
-function validaCodigo(codigo) {
+//function validaCodigo(codigo) {
 
     // cria a expressão regular. Poderíamos ter usado 
     // a sintaxe new RegExp(/\D{3}-\D{2}-\d{2}/)
     // \D é qualquer coisa não dígito
     // \D{3} é qualquer coisa não dígito que forme um grupo de 3 caracteres
     // \d é qualquer dígito.
-    let expressao = /\D{3}-\D{2}-\d{2}/; 
+    //let expressao = /^\D{3}-\D{2}-\d{2}$/; vai sempre te pedir 3 2 2 de cada coisa
 
     // toda expressão regular possui o método test 
     // que recebe o alvo do teste, retornando true
     // se passou, e false se falhou
-    if(expressao.test(codigo)) {
-          alert('senha na formação correta');
-          console.log(codigo)
-      } 
-      else {
-          alert('escreva a senha da forma correta');
-      }
+    //if(expressao.test(codigo)) {
+          //alert('senha na formação correta');
+          //console.log(codigo)
+      //} 
+      //else {
+      //    alert('escreva a senha da forma correta');
+      //}
 
-}
+//}
 
 //validaCodigo('GWZ-JJ-12'); // válido
 //validaCodigo('1X1-JJ-12'); // inválido
