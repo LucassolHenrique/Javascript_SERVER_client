31248f133be40a4c4fef12f09e25d090c674980c
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2024-08-28 14:51:41 -0300

    aa
    aa

---
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Jogo dos numero tem que acertar o numero');
+let numeroSecreto = 29; //variavel
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if(numeroSecreto == chute) {
+        alert('O n');
+        return;
+} 
