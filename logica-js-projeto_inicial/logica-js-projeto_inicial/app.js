e8b04f9f4eef338d898795d57eb4a964b67b8672
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2024-09-02 15:10:02 -0300

    a

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,13 +1,58 @@
 
-alert('Jogo dos numero tem que acertar o numero');
+let numeronegativo = 10;
+let numeropositivo = 1;
 
-let numeroSecreto = 29; //variavel
-
-let chute = prompt('Escolha um número entre 1 e 30');
-
-if(chute == numeroSecreto) {
-        alert('acertou ' + numeroSecreto, 'ola' + chute);
-} else {
-        alert('Vocé errou');
+while (numeronegativo != 0) {
+        console.log('de 10 a 1 ' + numeronegativo);
+        numeronegativo--;
 }
+console.log(`--------------------------------`);
+
+while (numeropositivo != 11) {
+        console.log('de 1 a 11 ' + numeropositivo);
+        numeropositivo++;
+}
+
+
+
+
+
+let rand;
+
+rand = prompt('Jogo dos numero tem que acertar o numero, escolha quantos numeros quer');
+
+let numeroSecreto = parseInt(Math.random() * rand + 1); //variavel
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+let contador = 1;
+
+
+
+while (chute != numeroSecreto) {
+        chute = prompt(`Escolha um numero entre 1 e 100`);
+        if (chute == numeroSecreto) {
+                break;
+        } else {
+                if (chute > numeroSecreto) {
+                        alert(`O número secreto é menor que ${chute}`);
+                } else {
+                        alert(`O número secreto é maior que ${chute}`);
+                }
+                tentativas++;
+        }
+
+}
+
+//esta criando uma norma dentro do let palavraTentativa (caso tentativas maior que 1 algo acontece)
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        alert('o numero era ' + numeroSecreto + ' o total de ' + tentativas + ' numero de ' + palavraTentativa);
+
+
+// if (tentativas > 1) {
+//         alert('acertou ' + numeroSecreto + ' ola ' + chute + ' numero de tentativas ' + tentativas);
+// } else {
+//         alert('acertou ' + numeroSecreto + ' ola ' + chute + ' numero de tentativa ' + tentativas);
+// }
 
--- testejava/1-console.js
+++ testejava/1-console.js
@@ -1,6 +1,5 @@
 console.log("trabalhando com ");
 
-const idade = 16;
 const nome = "Lucas";
 const sobrenome = "Sol";
 const somaidade = idade+2;
@@ -8,3 +7,29 @@ const somaidade = idade+2;
 console.log(nome + "" + sobrenome)
 console.log(nome, sobrenome)
 console.log(`class ${nome}`);
+
+
+
+let idade = prompt("Digite sua idade:");
+
+if (idade >= 18) {
+
+  console.log("Você é maior de idade.");
+} else {
+  console.log("Você é menor de idade.");
+}
+
+
+let porcentagemDesconto = 0;
+let quantidadeMilhas = 6000;
+
+if(quantidadeMilhas >= 30000){
+    porcentagemDesconto = porcentagemDesconto + 20;
+} else {
+    if(quantidadeMilhas > 5000){
+    porcentagemDesconto = porcentagemDesconto + 10;
+}
+
+}
+
+console.log(porcentagemDesconto, quantidadeMilhas);
