dde039e1a1ebee497458e3b879ee9db29e2fb8f0
Lucas sol <porquenpode@gmail.com>
2025-06-10 16:07:15 -0300

    revisando

--- Orientação_objeto/index.js
+++ Orientação_objeto/index.js
@@ -7,6 +7,8 @@ class Cliente {
 }
 
 class ContaCorrente{
+    agencia;
+    saldo;
 
 }
 
@@ -14,28 +16,20 @@ class ContaCorrente{
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
-
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456;
 
 cliente2.nome = "alice";
 cliente2.cpf = 8882223309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
 const cliente3Nome = "alice";
+
+
+
 const cliente3CPF = 88822233309;
-const clienteRG = assa
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
 
-
-console.log(cliente1Nome);
-
+console.log(cliente1);
+console.log(cliente2);
 
 
 
