96091872999e4578addb3a7e538d00128e470f03
Lucas sol <93673324+LucassolHenrique@users.noreply.github.com>
2025-10-21 19:51:06 -0300

    subindo.

--- Orientação_objeto/index.js
+++ Orientação_objeto/index.js
@@ -1,3 +1,25 @@
-//26/08/2025 aula de JS 
+//07/10/2025
 
+const num1 = 2;
+const num2 = 4;
+const operacao = 'soma'; 
 
+if (operacao === 'soma') {
+    console.log(num1 + num2);
+} else if (operacao == 'multiplicação'){
+    console.log(num1 * num2);
+} else {
+    console.log('Operação não indentificada');
+}
+
+const salario = 12000;
+
+if (salario >= 11000) {
+    console.log('3% de bônus');
+} else if (salario < 11000 && salario >= 7000){
+    console.log('5% de bônus');
+} else if (salario > 7000 && salario >= 4000) {
+    console.log('7% de Bônus ');
+} else {
+    console.log('9% de bônus');
+}
